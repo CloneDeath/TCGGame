f3411f33efed21296489e23f3d58dd472f05d06e
Nicholas Rodine <halfofastaple@gmail.com>
2025-07-11 15:12:11 -0500

    Merge master

--- Main.gd
+++ Main.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
 var battlefield_scene := preload("res://Scenes/Battlefield.tscn")
+const TurnTracker = preload("res://TurnTracker.gd")
+
+var turn_tracker: TurnTracker
 
 var player_resources := {
     "Wood": 0,
@@ -16,6 +19,18 @@ func _ready():
     var battlefield = battlefield_scene.instantiate()
     add_child(battlefield)
     update_resource_labels()
+
+    turn_tracker = TurnTracker.new()
+    add_child(turn_tracker)
+    turn_tracker.connect("turn_changed", _on_turn_changed)
+    _on_turn_changed(turn_tracker.current_player)
+
+func _input(event):
+    if event.is_action_pressed("ui_accept"):
+        turn_tracker.next_turn()
+
+func _on_turn_changed(player):
+    print("Player %d's turn" % (player + 1))
 
 func update_resource_labels():
     $PlayerResources/PlayerLabel.text = "Wood: %d | Gold: %d" % [
